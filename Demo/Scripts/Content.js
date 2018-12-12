8b99f38cbd51bdf9746c1d3698f94528d888db2b
Vjeko <vjeko@vjeko.com>
2018-12-12 11:03:40 +0100

    Using DOM instead of jQuery

--- Demo/Scripts/Content.js
+++ Demo/Scripts/Content.js
@@ -1,11 +1,15 @@
 (function ($) {
     $(document).ready(function () {
         var html = Microsoft.Dynamics.NAV.GetImageResource("Demo/Html/Control.html");
-        $("#controlAddIn").load(html, function() {
-            $("#controlAddIn").find("img").each(function() {
-                var src = Microsoft.Dynamics.NAV.GetImageResource($(this).attr("src"));
-                $(this).attr("src", src);
-            });
+        $("#controlAddIn").load(html, function () {
+            var imgs = document
+                .getElementById("controlAddIn")
+                .getElementsByTagName("img");
+            for (var i = 0; i < imgs.length; i++) {
+                var src = Microsoft.Dynamics.NAV.GetImageResource(
+                    imgs[i].getAttribute("src"));
+                imgs[i].src = src;
+            }
         });
     });
 })(jQuery);
