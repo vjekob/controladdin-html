c1921f3ff926723074a4608cc7f948d4688e37e6
Vjeko <vjeko@vjeko.com>
2018-12-12 10:56:47 +0100

    Fixing src

--- Demo/Scripts/Content.js
+++ Demo/Scripts/Content.js
@@ -1,6 +1,11 @@
 (function ($) {
     $(document).ready(function () {
         var html = Microsoft.Dynamics.NAV.GetImageResource("Demo/Html/Control.html");
-        $("#controlAddIn").load(html);
+        $("#controlAddIn").load(html, function() {
+            $("#controlAddIn").find("img").each(function() {
+                var src = Microsoft.Dynamics.NAV.GetImageResource($(this).attr("src"));
+                $(this).attr("src", src);
+            });
+        });
     });
 })(jQuery);
